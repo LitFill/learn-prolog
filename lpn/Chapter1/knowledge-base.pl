0a64d17c1c3b03720898d47e0c04fb24a1d9e7a0
LitFill <marrazzy54@gmail.com>
2025-05-19 18:40:22 +0700

    todo: make a new syntax for prolog.

--- lpn/Chapter1/knowledge-base.pl
+++ lpn/Chapter1/knowledge-base.pl
@@ -1,47 +1,47 @@
 % woman(Name)
-woman(mia).
-woman(jody).
-woman(yolanda).
+    woman(mia).
+    woman(jody).
+    woman(yolanda).
 
 % there is party now.
 party.
 
 % happy(Name)
-happy(yolanda).
-happy(vincent).
-
-% listen_to_music(Name)
-listen_to_music(mia).
-
-listen_to_music(yolanda) :-
     happy(yolanda).
-
-listen_to_music(butch).
-
-% plays_guitar(Name)
-plays_guitar(jody).
-
-plays_guitar(mia)     :-
-    listen_to_music(mia).
-
-plays_guitar(yolanda) :-
-    listen_to_music(yolanda).
-
-plays_guitar(vincent) :-
-    listen_to_music(vincent),
     happy(vincent).
 
-plays_guitar(butch)   :-
-    happy(butch)
-;   listen_to_music(butch).
+% listen_to_music(Name)
+    listen_to_music(mia).
+
+    listen_to_music(yolanda) :-
+        happy(yolanda).
+
+    listen_to_music(butch).
+
+% plays_guitar(Name)
+    plays_guitar(jody).
+
+    plays_guitar(mia)     :-
+        listen_to_music(mia).
+
+    plays_guitar(yolanda) :-
+        listen_to_music(yolanda).
+
+    plays_guitar(vincent) :-
+        listen_to_music(vincent),
+        happy(vincent).
+
+    plays_guitar(butch)   :-
+        happy(butch)
+    ;   listen_to_music(butch).
 
 'plays guitar'(ama).
 
 % loves(Name1, Name2) = Name1 loves Name2
-loves(vincent,   mia).
-loves(marcellus, mia).
-loves(pumpkin,   honey).
-loves(honey,     pumpkin).
+    loves(vincent,   mia).
+    loves(marcellus, mia).
+    loves(pumpkin,   honey).
+    loves(honey,     pumpkin).
 
 % jealous(Name1, Name2) = Name1 and Name2 love the same person
 jealous(X, Y) :-
